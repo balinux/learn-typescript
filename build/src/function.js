690bebaf8f06411acd931d6cef061d3d1af8defb
balinux <ryojuniyantara@gmail.com>
2023-03-09 16:35:33 +0800

    function typescript

--- build/src/function.js
+++ build/src/function.js
@@ -1,5 +1,15 @@
 "use strict";
+// funtion in typescrit
 function getName(name) {
     return `hello ${name}`;
 }
 console.log('getName', getName("balinux"));
+function getAge(age) {
+    return age;
+}
+console.log('getAge', getAge(12));
+// function tidak mereturn data
+function printName() {
+    console.log('void function');
+}
+printName();
--- src/function.ts
+++ src/function.ts
@@ -1,5 +1,20 @@
+// funtion in typescrit
+
 function getName(name:string) {
     return `hello ${name}`
 }
 
 console.log('getName', getName("balinux"))
+
+function getAge(age:number) {
+    return age;
+}
+
+console.log('getAge', getAge(12))
+
+// function tidak mereturn data
+function printName():void {
+    console.log('void function');
+}
+
+printName()
