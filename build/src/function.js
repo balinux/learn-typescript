5259d7eeb6457f5d30152e79d6ad07d836249d87
balinux <ryojuniyantara@gmail.com>
2023-03-10 08:44:17 +0800

    13. Tutorial TypeScript : Function Default Parameter

--- build/src/function.js
+++ build/src/function.js
@@ -3,14 +3,14 @@
 function getName(name) {
     return `hello ${name}`;
 }
-console.log('getName', getName("balinux"));
+console.log("getName", getName("balinux"));
 function getAge(age) {
     return age;
 }
-console.log('getAge', getAge(12));
+console.log("getAge", getAge(12));
 // function tidak mereturn data
 function printName() {
-    console.log('void function');
+    console.log("void function");
 }
 printName();
 // argunment
@@ -18,10 +18,21 @@ function multiplyNumber(num1, num2) {
     return num1 * num2;
 }
 const result = multiplyNumber(2, 5);
-console.log('result', result);
+console.log("result", result);
 // ini cara implemetasi type funtion pada function add
 const add = (va1, val2) => {
     return va1 + val2;
 };
 // cara  implementasi
-console.log('add', add(12, 23));
+console.log("add", add(12, 23));
+/**
+ * default parameter
+ * di variable last merupakan contoh penggunaan defaultparameter
+ */
+const fullname = (first, last = "juniyantara") => {
+    return first + " " + last;
+};
+let fullName = fullname("rio");
+let fullName2 = fullname("khalid", "putra");
+console.log(fullName);
+console.log(fullName2);
--- src/function.ts
+++ src/function.ts
@@ -1,49 +1,64 @@
 // funtion in typescrit
 
 function getName(name: string) {
-    return `hello ${name}`
+  return `hello ${name}`;
 }
 
-console.log('getName', getName("balinux"))
+console.log("getName", getName("balinux"));
 
 function getAge(age: number) {
-    return age;
+  return age;
 }
 
-console.log('getAge', getAge(12))
+console.log("getAge", getAge(12));
 
 // function tidak mereturn data
 function printName(): void {
-    console.log('void function');
+  console.log("void function");
 }
 
-printName()
-
+printName();
 
 // argunment
 
 function multiplyNumber(num1: number, num2: number): number {
-    return num1 * num2
+  return num1 * num2;
 }
 
-const result = multiplyNumber(2, 5)
+const result = multiplyNumber(2, 5);
 
-console.log('result', result)
-
+console.log("result", result);
 
 /**
  * function sebagai type data
- * Tammbah : merupakan nama Typedata 
+ * Tammbah : merupakan nama Typedata
  * (va1: number, val2: number) ; merupakan parameter dan type parameter
  * number: merupakan kembalian/return data
- * 
+ *
  */
 type Tambah = (va1: number, val2: number) => number;
 
 // ini cara implemetasi type funtion pada function add
 const add: Tambah = (va1: number, val2: number): number => {
-    return va1 + val2
-}
+  return va1 + val2;
+};
 
 // cara  implementasi
-console.log('add', add(12, 23))
+console.log("add", add(12, 23));
+
+/**
+ * default parameter
+ * di variable last merupakan contoh penggunaan defaultparameter
+ */
+
+const fullname = (first: string, last: string = "juniyantara"): string => {
+  return first + " " + last;
+};
+
+let fullName: string = fullname("rio");
+let fullName2: string = fullname("khalid", "putra");
+
+console.log(fullName);
+console.log(fullName2);
+
+
