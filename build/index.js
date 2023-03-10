4a65845d1e06ea74998be4f0c0cfdf26c2fb0999
balinux <ryojuniyantara@gmail.com>
2023-03-10 08:37:35 +0800

    12. Tutorial TypeScript : Tipe Data Union

--- build/index.js
+++ build/index.js
@@ -1,15 +1,21 @@
 "use strict";
 // type string untuk menulis data string
 let home = "rio";
-console.log('home', home);
+console.log("home", home);
 // number untuk data number
 let numberData = 12;
-console.log('numberData', numberData);
-// boolean: 
+console.log("numberData", numberData);
+// boolean:
 let isMaarried;
 isMaarried = true;
-console.log('isMaarried', isMaarried);
+console.log("isMaarried", isMaarried);
 // any semua data
 let randomData = "random";
 randomData = 23;
-console.log('randomData', randomData);
+console.log("randomData", randomData);
+/**
+ * type union untuk handle seperti nomor hp dengan kode negara
+ */
+let phone;
+phone = 62816123;
+phone = "082123123";
--- index.ts
+++ index.ts
@@ -1,18 +1,25 @@
-
 // type string untuk menulis data string
-let home:string = "rio";
-console.log('home', home)
+let home: string = "rio";
+console.log("home", home);
 
 // number untuk data number
 let numberData: number = 12;
-console.log('numberData', numberData)
+console.log("numberData", numberData);
 
-// boolean: 
-let isMaarried:boolean
+// boolean:
+let isMaarried: boolean;
 isMaarried = true;
-console.log('isMaarried', isMaarried)
+console.log("isMaarried", isMaarried);
 
 // any semua data
-let randomData:any = "random"
-randomData=23
-console.log('randomData', randomData)
+let randomData: any = "random";
+randomData = 23;
+console.log("randomData", randomData);
+
+/**
+ * type union untuk handle seperti nomor hp dengan kode negara
+ */
+
+let phone: number | string;
+phone = 62816123;
+phone = "082123123";
