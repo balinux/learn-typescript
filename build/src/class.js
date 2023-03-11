92e84543dabe6351f9388bf51c891f1a55c99f6d
balinux <ryojuniyantara@gmail.com>
2023-03-11 12:46:56 +0800

    16. Tutorial TypeScript : Inheritance

--- build/src/class.js
+++ build/src/class.js
@@ -3,11 +3,50 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 class User {
     constructor(name, age) {
+        this.getName = () => {
+            return this.name;
+        };
         this.name = name;
         this.age = age;
     }
+    setName(value) {
+        this.name = value;
+    }
 }
 exports.User = User;
-let user = new User("rio", 22);
-console.log(user.name);
-console.log(user.age);
+// let user = new User("rio", 22);
+// console.log(user.name);
+// console.log(user.age);
+/**
+ * class inheriten/turunan
+ * class ang mempunyai sifat turunan dari parent class
+ */
+class Admin extends User {
+    constructor() {
+        super(...arguments);
+        this.read = true;
+        this.write = true;
+    }
+    /**
+     * getRole merupakan nama function
+     * { read: boolean; write: boolean } : merupakan type return
+     * @returns
+     * return {
+          read: this.read,
+          write: this.write,
+        };
+        bagian ini meruapak returnnya
+     */
+    getRole() {
+        return {
+            read: this.read,
+            write: this.write,
+        };
+    }
+}
+// class admin butuh 2 variable karna mengikuti construct dari parent
+let admin = new Admin("rio", 22);
+console.log(admin.getName());
+console.log(admin.getRole());
+admin.setName("rian");
+console.log(admin.getName());
--- src/class.ts
+++ src/class.ts
@@ -1,13 +1,55 @@
-export class User{
-    public name:string
-    public age:number
+export class User {
+  public name: string;
+  public age: number;
 
-    constructor(name:string, age:number){
-        this.name = name
-        this.age = age
-    }
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  setName(value: string): void {
+    this.name = value;
+  }
+
+  getName = (): string => {
+    return this.name;
+  };
 }
 
-let user = new User("rio",22)
-console.log(user.name);
-console.log(user.age);
+// let user = new User("rio", 22);
+// console.log(user.name);
+// console.log(user.age);
+
+/**
+ * class inheriten/turunan
+ * class ang mempunyai sifat turunan dari parent class
+ */
+
+class Admin extends User {
+  read: boolean = true;
+  write: boolean = true;
+
+/**
+ * getRole merupakan nama function
+ * { read: boolean; write: boolean } : merupakan type return
+ * @returns 
+ * return {
+      read: this.read,
+      write: this.write,
+    };
+    bagian ini meruapak returnnya
+ */
+  getRole(): { read: boolean; write: boolean } {
+    return {
+      read: this.read,
+      write: this.write,
+    };
+  }
+}
+
+// class admin butuh 2 variable karna mengikuti construct dari parent
+let admin = new Admin("rio",22)
+console.log(admin.getName());
+console.log(admin.getRole());
+admin.setName("rian");
+console.log(admin.getName());
